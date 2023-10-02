a4ec0d82830f5fefa0f546e3fba6fa4ccc32fb31
mathurishani <91481581+mathurishani@users.noreply.github.com>
2023-10-02 07:25:29 +0530

    updates1

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-const container = document.querySelector(".container");
+// Handle Toggle Menu
+// Get the navigation links element
+var navLinks = document.getElementById("navLinks");
 
-document.querySelector(".open-navbar-icon").addEventListener("click", () => {
-  container.classList.add("change");
-});
+// Function to show the navigation menu
+function showMenu() {
+  navLinks.style.right = "0";
+}
 
-document.querySelector(".close-navbar-icon").addEventListener("click", () => {
-  container.classList.remove("change");
-});
+// Function to hide the navigation menu
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
 
-const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 
-let i = 0;
-
-Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
-  item.style.cssText = `background-color: ${colors[i++]}`;
-});
-
-Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
-  item.onclick = () => {
-    item.parentElement.parentElement.classList.toggle("change");
-  };
-});
-
+// Handle card click event to navigate to the target page
 function cardClick(event) {
   const card = event.target.closest(".card");
 
@@ -33,34 +25,42 @@ function cardClick(event) {
   }
 }
 
+
+// Validate the contact form
 function validate() {
-  let n = document.getElementById("name").value;
-  let e = document.getElementById("email").value;
-  let s = document.getElementById("subject").value;
-  let m = document.getElementById("message").value;
-  let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+  let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  
+  // Clear previous validation messages
   ["nvalidate", "evalidate", "svalidate", "mvalidate"].forEach((id) => {
     document.getElementById(id).innerHTML = "";
   });
+
   let isError = false;
-  if (n == "") {
+
+  if (name === "") {
     document.getElementById("nvalidate").innerHTML = "Please enter your name";
     isError = true;
   }
-  if (e == "") {
+
+  if (email === "") {
     document.getElementById("evalidate").innerHTML = "Please enter your email";
     isError = true;
-  } else if (!pattern.test(e)) {
+  } else if (!emailPattern.test(email)) {
     document.getElementById("evalidate").innerHTML = "Invalid email format.";
     isError = true;
   }
-  if (s == "") {
+
+  if (subject === "") {
     document.getElementById("svalidate").innerHTML = "Please enter subject";
     isError = true;
   }
-  if (m == "") {
-    document.getElementById("mvalidate").innerHTML =
-      "Please enter your message";
+
+  if (message === "") {
+    document.getElementById("mvalidate").innerHTML = "Please enter your message";
     isError = true;
   }
 
@@ -69,35 +69,43 @@ function validate() {
   }
 }
 
+
+// Validate login credentials
 function validateLogin() {
-  let u = document.getElementById("loginUsername").value;
-  let p = document.getElementById("loginPassword").value;
+  let username = document.getElementById("loginUsername").value;
+  let password = document.getElementById("loginPassword").value;
+
+  // Clear previous validation messages
   ["loginUsernameValidate", "loginPasswordValidate"].forEach((id) => {
     document.getElementById(id).innerHTML = "";
   });
+
   let isError = false;
-  if (u == "") {
-    document.getElementById("loginUsernameValidate").innerHTML =
-      "Please enter user name";
+
+  if (username === "") {
+    document.getElementById("loginUsernameValidate").innerHTML = "Please enter user name";
     isError = true;
   }
-  if (p == "") {
-    document.getElementById("loginPasswordValidate").innerHTML =
-      "Please enter password";
+
+  if (password === "") {
+    document.getElementById("loginPasswordValidate").innerHTML = "Please enter password";
     isError = true;
   }
 
   if (!isError) {
-    alert("LOGIN SUCCESSFUL. WELCOME " + u);
+    alert("LOGIN SUCCESSFUL. WELCOME " + username);
   }
 }
 
+// Validate registration details
 function validateRegistration() {
-  let u = document.getElementById("registerUsername").value;
-  let e = document.getElementById("email").value;
-  let p = document.getElementById("registerPassword").value;
-  let c = document.getElementById("confirmPassword").value;
-  let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  let username = document.getElementById("registerUsername").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("registerPassword").value;
+  let confirmPassword = document.getElementById("confirmPassword").value;
+  let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  // Clear previous validation messages
   [
     "registerUsernameValidate",
     "registerEmailValidate",
@@ -106,72 +114,49 @@ function validateRegistration() {
   ].forEach((id) => {
     document.getElementById(id).innerHTML = "";
   });
+
   let isError = false;
-  if (u == "") {
-    document.getElementById("registerUsernameValidate").innerHTML =
-      "Please enter user name";
+
+  if (username === "") {
+    document.getElementById("registerUsernameValidate").innerHTML = "Please enter user name";
     isError = true;
   }
-  if (e == "") {
-    document.getElementById("registerEmailValidate").innerHTML =
-      "Please enter email";
+
+  if (email === "") {
+    document.getElementById("registerEmailValidate").innerHTML = "Please enter email";
     isError = true;
-  } else if (!pattern.test(e)) {
-    document.getElementById("registerEmailValidate").innerHTML =
-      "Invalid email format.";
-    isError = true;
-  }
-  if (p == "") {
-    document.getElementById("registerPasswordValidate").innerHTML =
-      "Please enter password";
+  } else if (!emailPattern.test(email)) {
+    document.getElementById("registerEmailValidate").innerHTML = "Invalid email format.";
     isError = true;
   }
-  if (c == "") {
-    document.getElementById("registerCPasswordValidate").innerHTML =
-      "Please enter password again";
-    isError = true;
-  } else if (p !== c) {
-    document.getElementById("registerCPasswordValidate").innerHTML =
-      "Passwords do not match";
+
+  if (password === "") {
+    document.getElementById("registerPasswordValidate").innerHTML = "Please enter password";
     isError = true;
   }
+
+  if (confirmPassword === "") {
+    document.getElementById("registerCPasswordValidate").innerHTML = "Please enter password again";
+    isError = true;
+  } else if (password !== confirmPassword) {
+    document.getElementById("registerCPasswordValidate").innerHTML = "Passwords do not match";
+    isError = true;
+  }
+
   if (!isError) {
     alert("Registration Successful");
   }
 }
+
+
+// Toggle between register and login forms
 function showRegisterForm() {
   document.getElementById("loginForm").style.display = "none";
   document.getElementById("registerForm").style.display = "flex";
 }
+
 function showLoginForm() {
   document.getElementById("loginForm").style.display = "flex";
   document.getElementById("registerForm").style.display = "none";
 }
 
-function filterContent() {
-  const searchInput = document.getElementById("searchInput");
-  const filter = searchInput.value.toLowerCase();
-  const contentList = document.getElementById("contentList");
-  const items = contentList.getElementsByTagName("li");
-
-  for (const item of items) {
-    const text = item.textContent.toLowerCase();
-    if (text.includes(filter)) {
-      item.style.display = "";
-    } else {
-      item.style.display = "none";
-    }
-  }
-}
-
-function performSearch() {
-  const searchInput = document.getElementById("searchInput");
-  const searchTerm = searchInput.value.trim();
-
-  if (searchTerm !== "") {
-    // For demonstration purposes, an alert is shown with the search term
-    alert(`Performing search for: ${searchTerm}`);
-  } else {
-    alert("Please enter a search term.");
-  }
-}
